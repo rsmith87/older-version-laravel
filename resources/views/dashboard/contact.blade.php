e2e94b8df918e0704aab1ea2c235ba0259aaa537
rsmith76633 <rsmith76633@gmail.com>
2018-04-06 18:10:50 -0400

    add button to relate contact/client to case

--- resources/views/dashboard/contact.blade.php
+++ resources/views/dashboard/contact.blade.php
@@ -6,8 +6,11 @@
   <nav class="nav nav-pills">
     <a class="nav-item nav-link btn btn-info" data-toggle="modal" data-target="#contact-modal" href="#"><i class="fas fa-plus"></i> <i class="fas fa-briefcase"></i> Edit client</a>
 		<a class="nav-item nav-link btn btn-info" data-toggle="modal" data-target="#contacts-modal" href="#"><i class="fa fa-plus"></i> <i class="fa fa-user"></i> Add client</a>		
- 		<a class="nav-item nav-link btn btn-info" data-toggle="modal" data-target="#contacts-modal" href="#"><i class="fa fa-plus"></i> <i class="fa fa-user"></i> Relate Client to Case</a>		
- 
+ 		@if($contact->is_client === 1 )
+		<a class="nav-item nav-link btn btn-info" data-toggle="modal" data-target="#contact-relate-case-modal" href="#"><i class="fa fa-user"></i> <i class="fa fa-plus"></i> <i class="fa fa-briefcase"></i> Relate client to case</a>		
+ 		@else
+		<a class="nav-item nav-link btn btn-info" data-toggle="modal" data-target="#contact-relate-case-modal" href="#"><i class="fa fa-user"></i> <i class="fa fa-plus"></i> <i class="fa fa-briefcase"></i> Relate contact to case</a>				
+		@endif
 	</nav>  	
 
 	
@@ -107,88 +110,72 @@
 
 	
 
-       <div class="modal fade" id="contact-modal">
-         <div class="modal-dialog modal-lg">
-    		 <div class="modal-content">
-         <div class="modal-body">
-           <h3>
-             <i class="fas fa-address-card"></i> Add a contact
-           </h3>
-           <div class="clearfix"></div>
-           <hr />
-             <form method="post" action="/dashboard/contacts/add">
-           <input type="hidden" name="id" value="{{ $contact->id }}">
-          <input type="hidden" name="_token" value="{{ csrf_token() }}">
-							 <input type="hidden" name="is_client" value="{{ $is_client }}" />
-               
+<div class="modal fade" id="contact-modal">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-body">
+				<h3>
+				<i class="fas fa-address-card"></i> Add a contact
+				</h3>
+				<div class="clearfix"></div>
+				<hr />
+					<form method="post" action="/dashboard/contacts/add">
+						<input type="hidden" name="id" value="{{ $contact->id }}">
+						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+						<input type="hidden" name="is_client" value="{{ $is_client }}" />
 
-           <div class="col-sm-6 col-xs-12">
-             
-               
-                 <label>First name</label>
-               
-               <input type="text" class="form-control" value="{{ $contact->first_name }}" name="first_name" aria-label="First Name">
-           </div>  
-           <div class="col-sm-6 col-xs-12">
-             
-               
-                 <label>Last name</label>
-               <input type="text" class="form-control" value="{{ $contact->last_name }}" name="last_name" aria-label="Last Name">
-           </div>                
-           <div class="col-sm-6 col-xs-12">
-             
-               
-                 <label>Company</label>
-               <input type="text" class="form-control"  value="{{ $contact->company }}" name="company" aria-label="Company">
-           </div> 
-           <div class="col-sm-6 col-xs-12">
-             
-               
-                 <label>Company title</label>
-               <input type="text" class="form-control" value="{{ $contact->company_title }}" name="company_title" aria-label="Company title">
-           </div> 
-           <div class="col-sm-6 col-xs-12">
-             
-               
-                 <label>Phone</label>
-               <input type="text" class="form-control" id="phone" value="{{ $contact->phone }}" name="phone" aria-label="Phone">
-           </div>     
-           <div class="col-sm-6 col-xs-12">
-             
-               
-                 <label>Email</label>
-               <input type="text" class="form-control" value="{{ $contact->email }}" name="email" aria-label="Email">
-           </div> 
-           <div class="col-sm-6 col-xs-12">
-             
-               
-                 <label>Address</label>
-               <input type="text" class="form-control" value="{{ $contact->address }}" name="address" aria-label="Address">
-           </div> 
-           <div class="col-sm-6 col-xs-12">
-             
-               
-             <label>Case</label>
-							 <input type="hidden" name="case_id" value="{{ !empty($contact->case_id) ? $contact->case_id : '' }}" />		
-						 
-               <input type="text" name="case_name" value="{{ $contact->case_id != 0 || !empty($array_cases[$contact->case_id]) ? $array_cases[$contact->case_id] : '' }}" class="form-control" />
-             </div>
-           </div>   
 
-      	<div class="col-sm-12">
-         <button class="btn btn-primary mt-1 mb-5">
-           <i class="fas fa-check"></i> Submit
-         </button
-					 </div>
-       
-                            </form>
-       </div>
-      
-						 
-						 
-						 </div>
-       </div>
+						<div class="col-sm-6 col-xs-12">
+							<label>First name</label>
+							<input type="text" class="form-control" value="{{ $contact->first_name }}" name="first_name" aria-label="First Name">
+						</div> 
+						
+						<div class="col-sm-6 col-xs-12">
+							<label>Last name</label>
+							<input type="text" class="form-control" value="{{ $contact->last_name }}" name="last_name" aria-label="Last Name">
+						</div> 
+						
+						<div class="col-sm-6 col-xs-12">
+							<label>Company</label>
+							<input type="text" class="form-control"  value="{{ $contact->company }}" name="company" aria-label="Company">
+						</div> 
+						
+						<div class="col-sm-6 col-xs-12">
+							<label>Company title</label>
+							<input type="text" class="form-control" value="{{ $contact->company_title }}" name="company_title" aria-label="Company title">
+						</div> 
+						
+						<div class="col-sm-6 col-xs-12">
+							<label>Phone</label>
+							<input type="text" class="form-control" id="phone" value="{{ $contact->phone }}" name="phone" aria-label="Phone">
+						</div> 
+						
+						<div class="col-sm-6 col-xs-12">
+							<label>Email</label>
+							<input type="text" class="form-control" value="{{ $contact->email }}" name="email" aria-label="Email">
+						</div> 
+						
+						<div class="col-sm-6 col-xs-12">
+							<label>Address</label>
+							<input type="text" class="form-control" value="{{ $contact->address }}" name="address" aria-label="Address">
+						</div> 
+						
+						<div class="col-sm-6 col-xs-12">
+							<label>Case</label>
+							<input type="hidden" name="case_id" value="{{ !empty($contact->case_id) ? $contact->case_id : '' }}" />		
+							<input type="text" name="case_name" value="{{ $contact->case_id != 0 || !empty($array_cases[$contact->case_id]) ? $array_cases[$contact->case_id] : '' }}" class="form-control" />
+						</div>
+						
+						<div class="col-sm-12">
+							<button class="btn btn-primary mt-1 mb-5">
+								<i class="fas fa-check"></i> Submit
+							</button
+						</div>
 
+					</form>
+			</div>
+		</div>
+	</div>
 </div>
 
 <script src="{{ asset('js/autocomplete.js') }}"></script>
