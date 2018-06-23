d073d01489fae61d39d2ee6c52188a1de0069eb2
Robert Smith <codenut33@gmail.com>
2018-06-23 17:34:21 -0500

    cleaned up composer - dev dependencies are marked in require-dev.  kernal updates for filemanager middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,7 +28,9 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,        
+            \App\Http\Middleware\VerifyCsrfToken::class,   
+            \App\Http\Middleware\CreateDefaultFolder::class,
+            \App\Http\Middleware\MultiUser::class,
         ],
 
         'api' => [
