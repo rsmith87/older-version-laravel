2c2cbea68eb7e6021e393bdc6f0ae3ae8d8669d9
Robert Smith <codenut33@gmail.com>
2018-06-23 23:15:49 -0500

    try fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,7 +29,6 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,   
-            \App\Http\Middleware\CreateDefaultFolder::class,
         ],
 
         'api' => [
@@ -45,11 +44,11 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'isVerified' => \Jrean\UserVerification\Middleware\IsVerified::class,
         'lock' => \App\Http\Middleware\LockAccount::class,
-        'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,        
     ];
 }
